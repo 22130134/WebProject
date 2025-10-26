3a58798981f98a2e37ab263618348fe3b62ee1c7
Hung Truong <23130130@st.hcmuaf.edu.vn>
2025-10-26 14:38:42 +0700

    Tạo giao diện User Page (HTML + CSS)

--- style/header.js
+++
@@ -1,6 +0,0 @@
-async function loadHeader() {
-    const res = await fetch('/style/header.html');
-    const html = await res.text();
-    document.querySelector('#header').innerHTML = html;
-}
-loadHeader();
