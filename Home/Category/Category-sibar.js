d6d80f02e0a45ded17e1fdeab7a085e0f4c5c6e6
huynh0801 <huynh080104@gmail.com>
2025-11-22 08:45:04 +0700

    thay doi header

--- Home/Category/Category-sibar.js
+++ Home/Category/Category-sibar.js
@@ -6,12 +6,12 @@
         { name: "Y tế gia đình",     icon: "https://sieuthiyte.com.vn/data/images/icon-menu-mobile/icon-new-2-1/Y-te-gia-dinh.png" },
         { name: "Y tế chuyên dụng",  icon: "https://sieuthiyte.com.vn/data/images/icon-menu-mobile/icon-new-2-1/Y-te-chuyen-dung.png"},
         { name: "Chăm sóc sắc đẹp",  icon: "https://sieuthiyte.com.vn/data/images/icon-menu-mobile/icon-new-2-1/Cham-soc-sac-dep.png" },
-        { name: "Chăm sóc sức khỏe", icon: "https://sieuthiyte.com.vn/data/images/icon-menu-mobile/icon-new-2-1/Cham-soc-suc-khoe.png" },
         { name: "Thiết bị gia đình", icon: "https://sieuthiyte.com.vn/data/images/icon-menu-mobile/icon-new-2-1/Thiet-bi-gia-dinh.png" },
         { name: "Đồ dùng mẹ và bé",  icon: "https://sieuthiyte.com.vn/data/images/icon-menu-mobile/icon-new-2-1/Do-dung-me-be.png" },
         { name: "Đồ thể thao",       icon: "https://sieuthiyte.com.vn/data/images/icon-menu-mobile/icon-new-2-1/Do-the-thao.png" },
         { name: "Quà tặng",          icon: "https://sieuthiyte.com.vn/data/images/San-Pham/icon-qua-tang-phong-thuy-70x70.png" },
-        { name: "Flash Sale",        icon: "https://sieuthiyte.com.vn/themes/images/flash-sale-styt.png?t=1", promo: true }
+        { name: "Chăm sóc sức khỏe", icon: "https://sieuthiyte.com.vn/data/images/icon-menu-mobile/icon-new-2-1/Cham-soc-suc-khoe.png" },
+
     ];
 
     // Util: bỏ dấu + tạo slug
