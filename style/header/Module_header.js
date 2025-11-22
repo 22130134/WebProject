7fb7d36dce9a059827a92a92259f90da4809e67f
huynh0801 <huynh080104@gmail.com>
2025-11-22 09:10:47 +0700

    thay doi header

--- style/header/Module_header.js
+++ style/header/Module_header.js
@@ -1,18 +1,4 @@
 async function loadHeader() {
-    // 1) chèn HTML fragment
-    const res = await fetch('/style/header/header.html');
-    const html = await res.text();
-    const mount = document.querySelector('#header');
-    mount.innerHTML = html;
-
-    // 2) nạp CSS (chỉ 1 lần)
-    const cssPath = '/style/header/header.css';
-    if (!document.querySelector(`link[href="${cssPath}"]`)) {
-        const link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.href = cssPath;
-        document.head.appendChild(link);
-    }
 
     // 3) nạp JS và gọi init (chỉ 1 lần)
     await loadScriptOnce('/style/header/header.js');
