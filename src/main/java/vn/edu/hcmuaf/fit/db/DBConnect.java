01308c4981b315ada857bcf1d92f6a345a2b6a67
huynh0801 <huynh080104@gmail.com>
2025-11-29 17:45:13 +0700

    tạo kết nối tới database và demo thử catalog

---
+++ src/main/java/vn/edu/hcmuaf/fit/db/DBConnect.java
@@ -0,0 +1,33 @@
+package vn.edu.hcmuaf.fit.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnect {
+    private static String url = "jdbc:mysql://localhost:3306/dataweb";
+    private static String user = "root";
+    private static String pass = "12345";
+    private static Connection connection;
+
+    public static Connection get() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(url, user, pass);
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void main(String[] args) {
+        Connection conn = get();
+        if (conn != null) {
+            System.out.println("Kết nối thành công!");
+        } else {
+            System.out.println("Kết nối thất bại!");
+        }
+    }
+}
